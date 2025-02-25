bd6bb1a9e753247b8e0b6cc0977c773107ab27ae
zablon <zablonoigo51@gmail.com>
2025-02-25 19:09:15 +0300

    Add method to compute total storage used by a bucket

---
+++ storage_used_by_bucket.py
@@ -0,0 +1,31 @@
+import boto3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+region = "us-east-1"
+bucket_name = os.getenv("AWS_BUCKET_NAME")
+s3_client = boto3.client("s3", region_name=region)
+
+def bucket_storage(bucket_name):
+    total_bytes = 0
+    try:
+        response = s3_client.list_objects_v2(Bucket=bucket_name)
+
+        if "Contents" in response:
+            # Loop through each object in the bucket and add its size
+            for obj in response["Contents"]:
+                total_bytes += obj["Size"]
+            
+            return total_bytes
+        else:
+            print("Warning: No objects found in the bucket or the bucket does not exist!")
+            return 0
+
+    except Exception as e:
+        print(f"Error fetching storage details: {e}")
+        return 0
+
+storage_used = bucket_storage(bucket_name)
+print(f"Total storage used by the bucket: {storage_used} bytes")
