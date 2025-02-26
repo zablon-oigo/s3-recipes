b6d01e8c93dcde32673ef3cac003f8e38c8a9cbd
zablon <zablonoigo51@gmail.com>
2025-02-26 22:27:00 +0300

    Add access key

--- store_data.py
+++ store_data.py
@@ -7,7 +7,12 @@ load_dotenv()
 
 region = "us-east-1"
 bucket_name = os.getenv("AWS_BUCKET_NAME")
-s3_client = boto3.client("s3", region_name=region)
+s3_client = boto3.client(
+    "s3",
+    region_name=region,
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
 
 def store_data(bucket_name, key_name, json_data):
     try:
